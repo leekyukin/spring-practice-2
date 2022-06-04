a412a23bb2c47ac0c2820cb2c207be742f011b78
leekyukin <leekujin14@gmail.com>
2022-06-04 17:42:46 +0900

    ADD :: BaseTimeEntity (#2)

---
+++ jwt1/src/main/java/com/practice/jwt1/global/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.practice.jwt1.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
