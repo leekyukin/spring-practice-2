97764cb9b25a9138eaab4b945270c4c301fe3726
leekyukin <leekujin14@gmail.com>
2022-06-04 19:29:44 +0900

    FIX :: import

---
+++ jwt1/src/main/java/com/practice/jwt1/user/exception/UserNotFoundException.java
@@ -0,0 +1,13 @@
+package com.practice.jwt1.user.exception;
+
+
+import com.practice.jwt1.global.error.exception.ErrorCode;
+import com.practice.jwt1.global.error.exception.PracticeException;
+
+public class UserNotFoundException extends PracticeException {
+
+    public static UserNotFoundException EXCEPTION = new UserNotFoundException();
+
+    private UserNotFoundException() {super(ErrorCode.USER_NOT_FOUND);}
+
+}
