ad49b0e8e163a74bcced3cfefa9aee0d102e0124
leekyukin <leekujin14@gmail.com>
2022-06-04 19:11:20 +0900

    ADD :: Details and DetilsService (#2)

---
+++ jwt1/src/main/java/com/practice/jwt1/global/security/auth/AuthDetails.java
@@ -0,0 +1,50 @@
+package com.practice.jwt1.global.security.auth;
+
+import com.practice.jwt1.user.domain.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@RequiredArgsConstructor
+public class AuthDetails implements UserDetails {
+
+    private final User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singleton(() -> user.getAuthority().name());
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
---
+++ jwt1/src/main/java/com/practice/jwt1/global/security/auth/AuthDetailsService.java
@@ -0,0 +1,22 @@
+package com.practice.jwt1.global.security.auth;
+
+import com.practice.jwt1.user.domain.exception.UserNotFoundException;
+import com.practice.jwt1.user.domain.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+
+@RequiredArgsConstructor
+public class AuthDetailsService implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .map(AuthDetails::new)
+                .orElseThrow(() -> UserNotFoundException.EXCEPTION);
+    }
+}
