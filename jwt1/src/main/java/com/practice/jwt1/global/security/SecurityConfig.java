e388588286e7828c3716892ecb3a258f30156ed9
leekyukin <leekujin14@gmail.com>
2022-06-04 20:24:57 +0900

    ADD :: Password encod algorithem

--- jwt1/src/main/java/com/practice/jwt1/global/security/SecurityConfig.java
+++ jwt1/src/main/java/com/practice/jwt1/global/security/SecurityConfig.java
@@ -1,15 +1,21 @@
-package com.practice.jwt1.global.config;
+package com.practice.jwt1.global.security;
 
 import org.hibernate.StatelessSession;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() { return new BCryptPasswordEncoder();}
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
