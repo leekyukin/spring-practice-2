2853d717e9bd3ce64e88663ee6ea5394a7f90190
leekyukin <leekujin14@gmail.com>
2022-06-04 19:42:35 +0900

    ADD :: check user exists (#3)

---
+++ jwt1/src/main/java/com/practice/jwt1/user/facade/UserFacade.java
@@ -0,0 +1,18 @@
+package com.practice.jwt1.user.facade;
+
+import com.practice.jwt1.user.domain.repository.UserRepository;
+import com.practice.jwt1.user.exception.UserAlreadyExistsException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class UserFacade {
+
+    private final UserRepository userRepository;
+
+    public void checkUser(String email) {
+        userRepository.findByEmail(email)
+                .orElseThrow(() -> UserAlreadyExistsException.EXCEPTION);
+    }
+}
--- jwt1/src/main/java/com/practice/jwt1/user/service/UserService.java
+++ jwt1/src/main/java/com/practice/jwt1/user/service/UserService.java
@@ -1,6 +1,7 @@
 package com.practice.jwt1.user.service;
 
 import com.practice.jwt1.user.domain.repository.UserRepository;
+import com.practice.jwt1.user.facade.UserFacade;
 import com.practice.jwt1.user.presentation.dto.request.CreateUserRequestDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -12,10 +13,12 @@ import org.springframework.transaction.annotation.Transactional;
 @Slf4j
 public class UserService {
 
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
+    private final UserFacade userFacade;
 
     @Transactional
     public void signup(CreateUserRequestDto request) {
+        userFacade.checkUser(request.getEmail());
         userRepository.save(CreateUserRequestDto.toEntity(request));
     }
 }
