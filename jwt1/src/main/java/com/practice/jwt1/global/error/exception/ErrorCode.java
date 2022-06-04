f921d34c4b3d6d16f22d5437761547ad4d81e8c6
leekyukin <leekujin14@gmail.com>
2022-06-04 19:12:32 +0900

    ADD :: UserNotFound error and exception (#2)

--- jwt1/src/main/java/com/practice/jwt1/global/error/exception/ErrorCode.java
+++ jwt1/src/main/java/com/practice/jwt1/global/error/exception/ErrorCode.java
@@ -8,8 +8,9 @@ import lombok.Getter;
 public enum ErrorCode {
 
     INTERNAL_SERVER_ERROR(500, "서버에 문제 발생"),
-    BAD_REQUEST(400, "잘 못된 요청")
+    BAD_REQUEST(400, "잘 못된 요청"),
 
+    USER_NOT_FOUND(404, "사용자를 찾을 수 없음")
     ;
 
     private final int status;
