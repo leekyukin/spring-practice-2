25328a85a76daee80c3ef8fe0b459058e270003c
leekyukin <leekujin14@gmail.com>
2022-06-04 20:02:39 +0900

    ADD :: Security Config (#4)
    - session stateless
    - formLogin, httpBasic, csrf disable
    - /user/** permit
    - others authenticated

---
+++ jwt1/src/main/java/com/practice/jwt1/global/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.practice.jwt1.global.config;
+
+import org.hibernate.StatelessSession;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http
+                .httpBasic().disable()
+                .csrf().disable()
+                .formLogin().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                ;
+
+        http
+                .authorizeRequests()
+                .antMatchers("/user/**").permitAll()
+                .anyRequest().authenticated()
+        ;
+    }
+}
