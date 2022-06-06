c1233e7871198c63f4d6da3339e9bbff292ce5cb
leekyukin <leekujin14@gmail.com>
2022-06-06 19:58:40 +0900

    ADD :: Set JwtAuthenticationFilter

--- jwt1/src/main/java/com/practice/jwt1/global/security/SecurityConfig.java
+++ jwt1/src/main/java/com/practice/jwt1/global/security/SecurityConfig.java
@@ -1,6 +1,12 @@
 package com.practice.jwt1.global.security;
 
-import org.hibernate.StatelessSession;
+import com.practice.jwt1.domain.user.domain.type.Authority;
+import com.practice.jwt1.global.security.auth.AuthDetailsService;
+import com.practice.jwt1.global.security.jwt.JwtAuthenticationFilter;
+import com.practice.jwt1.global.security.jwt.JwtTokenProvider;
+import com.practice.jwt1.global.security.jwt.JwtValidateService;
+import com.practice.jwt1.global.utils.CookieUtil;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,10 +15,17 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final AuthDetailsService authDetailsService;
+    private final JwtValidateService jwtValidateService;
+    private final CookieUtil cookieUtil;
+    private final JwtTokenProvider jwtTokenProvider;
 
     @Bean
     public PasswordEncoder passwordEncoder() { return new BCryptPasswordEncoder();}
@@ -29,8 +42,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         http
                 .authorizeRequests()
-                .antMatchers("/user/**").permitAll()
+                .antMatchers("/user/login", "/user").permitAll()
+                .antMatchers("/user/check").hasRole(Authority.ROEL_USER.name())
                 .anyRequest().authenticated()
         ;
+
+        http
+                .addFilterBefore(
+                        new JwtAuthenticationFilter(
+                                cookieUtil, authDetailsService, jwtValidateService, jwtTokenProvider
+                        ),
+                        UsernamePasswordAuthenticationFilter.class
+                );
     }
 }
