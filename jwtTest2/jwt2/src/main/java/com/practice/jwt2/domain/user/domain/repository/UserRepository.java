dfddc87128a866631a23f713b6ff75075f61769c
leekyukin <leekujin14@gmail.com>
2022-06-08 18:07:34 +0900

    ADD :: method in repository

--- jwtTest2/jwt2/src/main/java/com/practice/jwt2/domain/user/domain/repository/UserRepository.java
+++ jwtTest2/jwt2/src/main/java/com/practice/jwt2/domain/user/domain/repository/UserRepository.java
@@ -3,6 +3,9 @@ package com.practice.jwt2.domain.user.domain.repository;
 import com.practice.jwt2.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
+    Optional<User> findByEmail(String email);
 }
