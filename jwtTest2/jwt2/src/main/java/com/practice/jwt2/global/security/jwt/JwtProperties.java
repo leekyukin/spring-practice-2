88ff7a4d8170631888cdb9748b6c09c5bbd4116e
leekyukin <leekujin14@gmail.com>
2022-06-08 18:09:19 +0900

    SET :: accessToken name, time

---
+++ jwtTest2/jwt2/src/main/java/com/practice/jwt2/global/security/jwt/JwtProperties.java
@@ -0,0 +1,6 @@
+package com.practice.jwt2.global.security.jwt;
+
+public class JwtProperties {
+    public static final long ACCESS_TOKEN_VALID_DATE = 30 * 60 * 1000L;
+    public static final String ACCESS_TOKEN_NAME = "accessToken";
+}
