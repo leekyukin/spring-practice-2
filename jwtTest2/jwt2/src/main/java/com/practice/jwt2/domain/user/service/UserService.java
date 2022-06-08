fb932cf413afb2eaed025e435fd2a84aa0ed8d56
leekyukin <leekujin14@gmail.com>
2022-06-08 18:06:07 +0900

    ADD :: login logic

--- jwtTest2/jwt2/src/main/java/com/practice/jwt2/domain/user/service/UserService.java
+++ jwtTest2/jwt2/src/main/java/com/practice/jwt2/domain/user/service/UserService.java
@@ -1,8 +1,10 @@
 package com.practice.jwt2.domain.user.service;
 
+import com.practice.jwt2.domain.user.domain.User;
 import com.practice.jwt2.domain.user.domain.repository.UserRepository;
 import com.practice.jwt2.domain.user.facade.UserFacade;
 import com.practice.jwt2.domain.user.presentation.dto.request.CreateUserRequestDto;
+import com.practice.jwt2.domain.user.presentation.dto.request.LoginUserRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,5 +20,11 @@ public class UserService {
     public void join(CreateUserRequestDto request) {
         userFacade.checkUser(request.getEmail());
         userRepository.save(request.toEntity());
+    }
+
+    @Transactional(readOnly = true)
+    public void login(LoginUserRequestDto request) {
+        User user = userFacade.findByEmail(request.getEmail());
+        userFacade.checkPassword(user.getPassword(), request.getPassword());
     }
 }
